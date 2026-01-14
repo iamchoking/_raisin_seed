b7d5f6d19e0427929a338a3f5cd5d80c8a5984e4
iamchoking <iamchoking247@gmail.com>
2026-01-14 19:32:50 +0900

    2026-01-14  working state

--- config/settings/bashrc_add.sh
+++ config/settings/bashrc_add.sh
@@ -5,8 +5,8 @@ export RAISIN_MASTER_DIR="/home/$(whoami)/raisin_master"
 alias ldp='source $RAISIN_MASTER_DIR/ld_prefix_path.sh'
 alias raisin_setup='cd $RAISIN_MASTER_DIR && python3 raisin.py setup'
 
-alias raisin_gui='cd $RAISIN_MASTER_DIR && source ./ld_prefix_path.sh && ./cmake-build-release/src/raisin_gui/raisin_gui/raisin_gui'
-alias raipal_node='cd $RAISIN_MASTER_DIR && source ./ld_prefix_path.sh && ./cmake-build-release/src/raisin_raipal/raisin_raipal_node'
-alias raibo2_node='cd $RAISIN_MASTER_DIR && source ./ld_prefix_path.sh && ./install/bin/raisin_raibo2_node'
+alias raisin_gui='cd $RAISIN_MASTER_DIR && source ./ld_prefix_path.sh && sudo ./cmake-build-release/src/raisin_gui/raisin_gui/raisin_gui'
+alias raipal_node='cd $RAISIN_MASTER_DIR && source ./ld_prefix_path.sh && sudo ./cmake-build-release/src/raisin_raipal/raisin_raipal_node'
+alias raibo2_node='cd $RAISIN_MASTER_DIR && source ./ld_prefix_path.sh && sudo ./install/bin/raisin_raibo2_node'
 
 # <<< _raisin_seed <<<
